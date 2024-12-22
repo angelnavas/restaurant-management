d26de990113099387ff60be458cb0c654069769d
angelnavas <angel.navas.diaz@gmail.com>
2024-12-22 08:14:03 +0100

    feat: Add API routes configuration

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
-<?php
-
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+<?php 
+use Illuminate\Http\Request; 
+use Illuminate\Support\Facades\Route; 
+use App\Http\Controllers\RecipeController; 
+use App\Http\Controllers\ProductController; 
+use App\Http\Controllers\SaleController; 
+ 
+Route::prefix('api')- () { 
+    // Recipes 
+    Route::get('/recipes', [RecipeController::class, 'index']); 
+    Route::post('/recipes', [RecipeController::class, 'store']); 
+    Route::get('/recipes/analytics', [RecipeController::class, 'analytics']); 
+ 
+    // Products 
+    Route::get('/products', [ProductController::class, 'index']); 
+    Route::post('/products', [ProductController::class, 'store']); 
+ 
+    // Sales 
+    Route::post('/sales', [SaleController::class, 'store']); 
+    Route::get('/sales/analytics', [SaleController::class, 'analytics']); 
+}); 
