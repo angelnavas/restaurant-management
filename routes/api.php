3cb24f801be7a9cfda7bac8eb19a0a2ab22d3d95
angelnavas <angel.navas.diaz@gmail.com>
2024-12-22 08:18:59 +0100

    feat: Add API routes configuration

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,22 @@
-<?php 
-use Illuminate\Http\Request; 
-use Illuminate\Support\Facades\Route; 
-use App\Http\Controllers\RecipeController; 
-use App\Http\Controllers\ProductController; 
-use App\Http\Controllers\SaleController; 
- 
-Route::prefix('api')- () { 
-    // Recipes 
-    Route::get('/recipes', [RecipeController::class, 'index']); 
-    Route::post('/recipes', [RecipeController::class, 'store']); 
-    Route::get('/recipes/analytics', [RecipeController::class, 'analytics']); 
- 
-    // Products 
-    Route::get('/products', [ProductController::class, 'index']); 
-    Route::post('/products', [ProductController::class, 'store']); 
- 
-    // Sales 
-    Route::post('/sales', [SaleController::class, 'store']); 
-    Route::get('/sales/analytics', [SaleController::class, 'analytics']); 
-}); 
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RecipeController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\SaleController;
+
+Route::prefix('api')->group(function () {
+    // Recipes
+    Route::get('/recipes', [RecipeController::class, 'index']);
+    Route::post('/recipes', [RecipeController::class, 'store']);
+    Route::get('/recipes/analytics', [RecipeController::class, 'analytics']);
+
+    // Products
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+
+    // Sales
+    Route::post('/sales', [SaleController::class, 'store']);
+    Route::get('/sales/analytics', [SaleController::class, 'analytics']);
+});
