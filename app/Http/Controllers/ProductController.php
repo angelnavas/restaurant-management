896bb98951008c19b2d8e54a6764919414ed2f84
angelnavas <angel.navas.diaz@gmail.com>
2024-12-22 08:22:13 +0100

    feat: Add ProductController with basic CRUD

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,1 +1,26 @@
-<?php 
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return response()->json(['data' => Product::all()]);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'cost' => 'required|numeric|min:0',
+        ]);
+
+        $product = Product::create($validated);
+
+        return response()->json(['data' => $product], 201);
+    }
+}
