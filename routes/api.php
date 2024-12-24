6a15e89d264ad9b07a49696addcfff2b49ac87be
angelnavas <angel.navas.diaz@gmail.com>
2024-12-24 03:25:03 +0100

    routes

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,22 @@ use App\Http\Controllers\RecipeController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
 Route::prefix('api')->group(function () {
     // Recipes
     Route::get('/recipes', [RecipeController::class, 'index']);
